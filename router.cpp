fc68ab15c431644866f3e8120d85883e3277ba80
Jeff Hann <jeff.hann@redspace.com>
2015-02-13 14:13:40 -0400

    removing twitter

--- router.cpp
+++ router.cpp
@@ -1,6 +1,5 @@
 #include "router.h"
 #include "static.h"
-#include "twitter.h"
 #include <QVariant>
 #include <QDateTime>
 
@@ -10,12 +9,11 @@ Router::Router(QObject* parent)
 }
 
 void Router::service(HttpRequest& request, HttpResponse& response) {
-    Twitter twitter;
-    QString results = twitter.search();
 
     response.setHeader("Content-Type", "text/html; charset=ISO-8859-1");
     response.write("<html><body>");
-    response.write(results.toLocal8Bit().data());
+    response.write("Hello World");
+    //response.write(results.toLocal8Bit().data());
     response.write("</body></html>");
 
     qDebug("RequestMapper: finished request");
